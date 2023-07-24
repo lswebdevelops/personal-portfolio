07c5b00fea355070d9dc71f91cd5828cdc5fb381
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-07-24 13:25:48 -0300

    submitting data to the console

--- index.js
+++ index.js
@@ -4,5 +4,28 @@ function scrollToTop(){
         behavior: 'smooth',
     });
 }
+const button = document.getElementById("submit").addEventListener("click", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let message = document.getElementById('message').value;
+
+  // Check if all three fields have values
+  if (name && email && message) {
+    console.log({
+      "Name: ": name,
+      "Email: ": email,
+      "Message: ": message
+    });
+
+    alert("Your data has been submitted!");
+    // You can add further code here to send the form data to a server or perform any other actions needed.
+  } else {
+    alert("Please fill in all the required fields.");
+  }
+}
 
 
